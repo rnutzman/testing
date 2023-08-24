7d64db26c848bc1ff87de7729d58c67312fd2a7d
rnutzman <45185619+rnutzman@users.noreply.github.com>
2023-08-24 10:15:02 -0400

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,21 @@
+# VPC Variables
+variable "aws_subnets" {
+  type    = list
+  default = ["eks-subnet-1", "eks-subnet-2", "eks-subnet-3"]
+}
+
+variable "aws_subnet_cidr" {
+  default = {
+    "eks-subnet-1" = "10.0.1.0/24"
+    "eks-subnet-2" = "10.0.2.0/24"
+    "eks-subnet-3" = "10.0.3.0/24"
+  }
+}
+
+variable "aws_subnet_az" {
+  default = {
+    "eks-subnet-1" = "use2-az1"
+    "eks-subnet-2" = "use2-az2"
+    "eks-subnet-3" = "use2-az3"
+  }
+}
