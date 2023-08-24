ddc446b622c8a3578752626a01acc1df745bca41
rnutzman <45185619+rnutzman@users.noreply.github.com>
2023-08-24 10:55:15 -0400

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,9 @@ output "vpc_id" {
   description = "VPC ID"
   value = aws_vpc.eks-vpc.id
 }
+
+output "subnet_ids" {
+  description = "Subnet details"
+  value = [ for subnet in aws_subnet.eks-subnets: eks-subnets.name]
+}
+
